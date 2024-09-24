d132d802262943b11d21aaa21398bddc7cf7488f
krishdharwal <darkpirate47171@gmail.com>
2024-09-24 18:31:07 +0530

    jwt and kafka dependenci added

--- Spring Boot/src/main/java/Mail/Start_Mail.java
+++ Spring Boot/src/main/java/Mail/Start_Mail.java
@@ -2,10 +2,18 @@ package Mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class Start_Mail {
     public static void main(String[] args) {
         SpringApplication.run(Start_Mail.class,args);
+    }
+
+    @Bean
+    private JavaMailSender sender(){
+        return new JavaMailSenderImpl();
     }
 }
